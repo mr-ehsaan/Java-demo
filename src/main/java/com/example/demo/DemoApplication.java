f57bbb567170de0424d67633edf6160c92112e24
mr ehsaan <99403938+mr-ehsaan@users.noreply.github.com>
2023-07-26 09:14:33 -0400

    latest code with type<Set<T>> implementation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,25 +8,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.Iterator;
+import java.util.*;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class DemoApplication <T> {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
     }
 
 	
-    private final Map<String, Object> database = new HashMap<>();
+    private final Map<String, Set<T>> database = new HashMap<>();
 
 
     @PostMapping("/{dynamicKey}/add")
-      public ResponseEntity<String> add(@PathVariable String dynamicKey, @RequestBody Object value) {
-          database.put(dynamicKey, value);
+      public ResponseEntity<String> add(@PathVariable String dynamicKey, @RequestBody T value) {
+          Set<T> values = database.computeIfAbsent(dynamicKey, k -> new HashSet<>());
+          values.add(value);
           return ResponseEntity.ok("Object added to the collection with key: " + dynamicKey);
       }
   
@@ -36,38 +34,39 @@ public class DemoApplication {
           return ResponseEntity.ok("Collection cleared.");
       }
   
-    @GetMapping("/{dynamicKey}/contains")
-      public ResponseEntity<Boolean> contains(@PathVariable String dynamicKey) {
-          boolean containsKey = database.containsKey(dynamicKey);
+      @GetMapping("/contains/{key}")
+      public ResponseEntity<Boolean> contains(@PathVariable String key) {
+          boolean containsKey = database.containsKey(key);
           return ResponseEntity.ok(containsKey);
       }
   
-    @GetMapping("/isEmpty")
+      @GetMapping("/isEmpty")
       public ResponseEntity<Boolean> isEmpty() {
           boolean empty = database.isEmpty();
           return ResponseEntity.ok(empty);
       }
   
-    @GetMapping("/iterator")
-      public ResponseEntity<Iterator<Map.Entry<String, Object>>> iterator() {
-          Set<Map.Entry<String, Object>> entrySet = database.entrySet();
+      @GetMapping("/iterator")
+      public ResponseEntity<Iterator<Map.Entry<String, Set<T>>>> iterator() {
+          Set<Map.Entry<String, Set<T>>> entrySet = database.entrySet();
           return ResponseEntity.ok(entrySet.iterator());
       }
   
-    @DeleteMapping("/{dynamicKey}/remove")
-      public ResponseEntity<String> remove(@PathVariable String dynamicKey) {
-          if (database.containsKey(dynamicKey)) {
-              database.remove(dynamicKey);
+      @DeleteMapping("/{dynamicKey}/remove")
+      public ResponseEntity<String> remove(@PathVariable String dynamicKey, @RequestBody T value) {
+          Set<T> values = database.get(dynamicKey);
+          if (values != null && values.contains(value)) {
+              values.remove(value);
               return ResponseEntity.ok("Object removed from the collection with key: " + dynamicKey);
           } else {
               return ResponseEntity.notFound().build();
           }
       }
   
-    @GetMapping("/size")
+      @GetMapping("/size")
       public ResponseEntity<Integer> size() {
           int size = database.size();
           return ResponseEntity.ok(size);
       }
-  
+   
 }
